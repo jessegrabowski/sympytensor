948106bee3811810e4c160ddbf714a070c8b4f80
Jesse Grabowski <jessegrabowski@gmail.com>
2024-04-29 21:49:24 +0800

    Bump version 1.1.1 -> 1.2.1

--- sympytensor/__init__.py
+++ sympytensor/__init__.py
@@ -1,5 +1,5 @@
 from sympytensor.pymc import SympyDeterministic
 from sympytensor.pytensor import as_tensor, pytensor_function
 
-__version__ = "1.1.1`"
+__version__ = "1.2.1`"
 __all__ = ["as_tensor", "pytensor_function", "SympyDeterministic"]
