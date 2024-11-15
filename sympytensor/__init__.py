92749f314a3482c2b7e9849d5e6fa3d23ae335fc
Jesse Grabowski <48652735+jessegrabowski@users.noreply.github.com>
2024-11-15 17:19:09 +0100

    Add versioneer and release pipeline (#5)
    * Print transpose as `pt.matrix_transpose`
    
    * configure versioneer
    
    * configure versioneer
    
    * Add release job
    
    * update pyproject.toml

--- sympytensor/__init__.py
+++ sympytensor/__init__.py
@@ -1,5 +1,6 @@
+from sympytensor._version import get_versions
 from sympytensor.pymc import SympyDeterministic
 from sympytensor.pytensor import as_tensor, pytensor_function
 
-__version__ = "1.2.1`"
+__version__ = get_versions()["version"]
 __all__ = ["as_tensor", "pytensor_function", "SympyDeterministic"]
