d9ee2716b41c199f1d08791917f4c1706543f9c4
jessegrabowski <jessegrabowski@gmail.com>
2025-07-03 10:57:33 +0800

    fix typo

--- sympytensor/__init__.py
+++ sympytensor/__init__.py
@@ -2,5 +2,5 @@ from importlib.metadata import version
 from sympytensor.pymc import SympyDeterministic
 from sympytensor.pytensor import as_tensor, pytensor_function
 
-__version__ = version("pymc-extras")
+__version__ = version("sympytensor")
 __all__ = ["as_tensor", "pytensor_function", "SympyDeterministic"]
