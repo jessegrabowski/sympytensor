04d3054d7080731eb35482aded9910d68c2f0c15
jessegrabowski <jessegrabowski@gmail.com>
2026-05-15 11:04:50 -0500

    pre-commit

--- tests/test_pytensor.py
+++ tests/test_pytensor.py
@@ -945,6 +945,7 @@ def test_MatPow_zero_returns_identity():
     A_val = np.random.default_rng(0).standard_normal((3, 3))
     assert_allclose(f(A_val), np.eye(3))
 
+
 def test_Inverse():
     A = sp.MatrixSymbol("A", 3, 3)
     f = pytensor_function([A], [A.inv()])
@@ -961,7 +962,7 @@ def test_Inverse_times_vector():
     assert np.allclose(f(A_val, b_val), np.linalg.solve(A_val, b_val))
 
 
-def test_MatPow_negative_exponent_raises():
+def test_MatPow_large_exponent_uses_matrix_power():
     A = sp.MatrixSymbol("A", 3, 3)
     f = pytensor_function([A], [A**8], dims={A: 2})
     dot_nodes = [n for n in f.maker.fgraph.toposort() if "dot" in type(n.op).__name__.lower()]
